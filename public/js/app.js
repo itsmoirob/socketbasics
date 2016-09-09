024c1ee8bcdcb9cc5cafa2d5b0d9bb71775f4f46
Robbie Sherman <rob.w.sherman@gmail.com>
2016-09-09 17:32:29 +0100

    Added customer message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to socket.io server!');
+});
+
+socket.on('message', function (message) {
+    document.write('New message:</br>' );
+    document.write(message.text, '</br>');
 })
